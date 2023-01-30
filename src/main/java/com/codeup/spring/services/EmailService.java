b4e63c05bb06fe2b28fccc618b0371eb9e75f894
John-Michael Krsak <jmkrsak@gmail.com>
2023-01-29 23:44:00 -0500

    update

--- src/main/java/com/codeup/spring/services/EmailService.java
+++ src/main/java/com/codeup/spring/services/EmailService.java
@@ -7,7 +7,7 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-
+// refactor complete
 @Service("emailService")
 public class EmailService {
 
